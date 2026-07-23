97bf3f7e76e3caf85c943f15cad76fe0dc459d3b
alloo4 <darylopiyo7603@gmail.com>
2026-07-24 00:19:36 +0300

    Remove temporary diagnostic endpoint

--- api/diag.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-  res.status(200).json({
-    hasOidcToken: !!process.env.VERCEL_OIDC_TOKEN,
-    hasBlobStoreId: !!process.env.BLOB_STORE_ID,
-    hasBlobRwToken: !!process.env.BLOB_READ_WRITE_TOKEN,
-    hasBlobWebhookKey: !!process.env.BLOB_WEBHOOK_PUBLIC_KEY,
-    vercelEnv: process.env.VERCEL_ENV || null,
-  });
-};
